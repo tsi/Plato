4e53a12de9d965b351dd93d93117bfe26b8d0718
Tsachi Shlidor <tsi@322980.no-reply.drupal.org>
2012-07-08 12:44:27 +0300

    Quick tabs; Typography; and more...

--- scripts/plato.js
+++ scripts/plato.js
@@ -6,7 +6,30 @@
  */
 
 (function($) {
-  
+
+  Drupal.behaviors.categoryList = {
+    attach: function(context) {
+      $('.category-menu li').hover(
+        function() {
+          var level = $(this).parent().attr('class');
+          var start = level.indexOf('level-');
+          level = parseInt(level.substr(start+6, 1), 10) + 1;
+          $(this).children('ul.level-' + level).show('fast');
+        },
+        function() {
+          $(this).children('ul').hide('fast');
+        }
+      );
+    }
+  };
+
+  Drupal.behaviors.watcher = {
+    attach: function(context) {
+      $('.watcher_node:not(.watcher_node_watched) a').html(Drupal.t('Follow'));
+      $('.watcher_node.watcher_node_watched a').html(Drupal.t('Unfollow'));
+    }
+  };
+
   // DUPLICATE AND UNCOMMENT
   // Drupal.behaviors.behaviorName = {
   //   attach: function (context, settings) {
